ff149644a15b5f4468ab2f049d7aa17ebf145215
Pujan Pokhrel <ppokhrel@uno.edu>
2017-02-10 16:29:12 -0600

    changed branch

---
+++ apocalypse/Assets/player scripts/particle_collision/Nucleon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Nucleon class for spheres that will be attracted to the center of the scene
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour {
+
+    public float attractionForce;
+
+    Rigidbody body;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        body.AddForce(transform.localPosition * -attractionForce);
+    }
+
+  
+}
---
+++ apocalypse/Assets/player scripts/slotObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slotObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Collider[] findOverlap = Physics.OverlapSphere(this.transform.position, 10); 			//radius of capture = 10 units
+		float xoff = 6.25f;																		//offset = 6.25,7.02,-6.07
+		float yoff = 7.02f;
+		float zoff = -6.02f;
+
+		for (int i = 0; i < findOverlap.Length; i++) {
+			if (findOverlap [i].gameObject.tag == "symbol") {
+				findOverlap [i].gameObject.transform.position = new Vector3(this.transform.position.x + xoff, this.transform.position.y + yoff, this.transform.position.z + zoff);
+			}
+		}
+
+}
+}
